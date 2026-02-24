0929dbb3899c55173e1db8cbc1a0537265e54ecd
Ahmed Amir <na_ben_slimane@esi.dz>
2026-02-24 11:48:46 +0100

    Cleaned the project structure

--- test_api.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-import os
-
-genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
-
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(f"Model Name: {m.name}")
